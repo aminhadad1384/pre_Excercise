9e18d7dc0d8008aab9e81f8cca8ca5a149c87679
aminhadad84 <maminhadad84@gmail.com>
2026-08-28 11:07:59 +0330

    chore: initial Next.js setup with Tailwind

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Vazirmatn } from "next/font/google";
+
+const vazirmatn = Vazirmatn({ subsets: ["arabic"] });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +22,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
+      lang="fa"
+      dir="rtl"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body
+        className={`${vazirmatn.className} antialiased min-h-full flex flex-col`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
